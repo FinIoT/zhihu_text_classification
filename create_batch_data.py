cd31c2b30f2f084342a9e54983f193e5c57f9139
FinIoT <48702236@qq.com>
2018-05-21 21:11:05 +0800

    0521

--- create_batch_data.py
+++ create_batch_data.py
@@ -68,7 +68,7 @@ def wd_train_get_batch(title_len=30, content_len=150, batch_size=128):
     content=np.asarray(p.map(pad_X150,train_content))
     p.close()
     p.join()
-    X=np.hstack(title,content)
+    X=np.hstack([title,content])
     print('getting labels, this should cost several minutes, please wait...')
     
     y=get_labels()
@@ -96,6 +96,77 @@ def wd_train_get_batch(title_len=30, content_len=150, batch_size=128):
     sample_num = len(X_train)
     print('train_sample_num=%d' % sample_num)
     train_batch(X_train, y_train, wd_train_path, batch_size)
+
+def wd_test_get_batch(title_len=30, content_len=150, batch_size=128):
+    print('loading eval data...')
+    title=np.load('../data/ch_eval_title.npy')
+    content=np.load('../data/ch_eval_content.npy')
+    print('transfer the eval data to fixed title_length 30 and content_length 150...')
+    p=Pool()
+    title=np.asarray(p.map(pad_X30,title))
+    content=np.asarray(p.map(pad_X150,content))
+    p.close()
+    p.join()
+    X=np.hstack([title,content])
+    print('OK!transfered! titles and content are stacked horizontally')
+    print('test sample_num=',len(X))
+    eval_batch(X,wd_test_path,batch_size)
+    
+def ch_train_get_batch(title_len=52, content_len=300, batch_size=128):
+    print('loading char train_title and train_content.')
+    train_title = np.load('../data/ch_train_title.npy')
+    train_content = np.load('../data/ch_train_content.npy')
+    p = Pool()
+    X_title = np.asarray(p.map(pad_X52, train_title))
+    X_content = np.asarray(p.map(pad_X300, train_content))
+    p.close()
+    p.join()
+    X = np.hstack([X_title, X_content])
+    y = np.load('../data/y_tr.npy')
+    # 划分验证集
+    sample_num = X.shape[0]
+    np.random.seed(13)
+    valid_num = 100000
+    new_index = np.random.permutation(sample_num)
+    X = X[new_index]
+    y = y[new_index]
+    X_valid = X[:valid_num]
+    y_valid = y[:valid_num]
+    X_train = X[valid_num:]
+    y_train = y[valid_num:]
+    print('X_train.shape=', X_train.shape, 'y_train.shape=', y_train.shape)
+    print('X_valid.shape=', X_valid.shape, 'y_valid.shape=', y_valid.shape)
+    # 验证集打batch
+    print('creating batch data.')
+    sample_num = len(X_valid)
+    print('valid_sample_num=%d' % sample_num)
+    train_batch(X_valid, y_valid, ch_valid_path, batch_size)
+    # 训练集打batch
+    sample_num = len(X_train)
+    print('train_sample_num=%d' % sample_num)
+    train_batch(X_train, y_train, ch_train_path, batch_size)
+
+
+def ch_test_get_batch(title_len=52, content_len=300, batch_size=128):
+    eval_title = np.load('../data/ch_eval_title.npy')
+    eval_content = np.load('../data/ch_eval_content.npy')
+    p = Pool()
+    X_title = np.asarray(p.map(pad_X52, eval_title))
+    X_content = np.asarray(p.map(pad_X300, eval_content))
+    p.close()
+    p.join()
+    X = np.hstack([X_title, X_content])
+    sample_num = len(X)
+    print('eval_sample_num=%d' % sample_num)
+    eval_batch(X, ch_test_path, batch_size)
+
+if __name__ == '__main__':
+    wd_train_get_batch()
+    wd_test_get_batch()
+    ch_train_get_batch()
+    ch_test_get_batch()   
+    
+    
     
     
     
