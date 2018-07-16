09b2cedd6d5bd645cae1da29005785c5bce08030
FinIoT <liukesheng227@aliyun.com>
2018-07-16 08:47:52 -0500

    Add files via upload

--- imodels/wd_1_1_cnn_concat/network.py
+++ imodels/wd_1_1_cnn_concat/network.py
@@ -4,7 +4,6 @@ Created on Sun Jun  3 21:45:45 2018
 
 @author: LIKS
 """
-
 import tensorflow as tf
 
 """wd_1_1_cnn_concat
@@ -24,7 +23,7 @@ class Settings():
         self.ckpt_path='../../ckpt/'+self.model_name+'/'
         
 class TextCNN():
-        """
+    """
     title: inputs->textcnn->output_title
     content: inputs->textcnn->output_content
     concat[output_title, output_content] -> fc+bn+relu -> sigmoid_entropy.
@@ -59,7 +58,66 @@ class TextCNN():
             output_title=self.cnn_inference(self._X1_inputs, self.title_len)
         with tf.variable_scope('cnn_content'):
             output_content=self.cnn_inference(self._X2_inputs, self.content_len)
+        with tf.variable_scope('fc_bn_layer'):
+            output=tf.concate([output_title,output_content],axis=1)#batch_size*2560
+            W_fc=self.weight_variable([2*self.n_filter_total,self.fc_hidden_size],name='Weight_fc')
+            tf.summary.histogram('Weight_fc',W_fc)
             
+            h_fc=tf.matmul(output,W_fc,name='h_fc')#batch_size*fc_hidden_size
+            beta_fc=tf.Variable(tf.constant(0.1,tf.float32,shape=[self.fc_hidden_size],name='beta_fc'))
+            tf.summary.histogram('beta_fc',beta_fc)
+            fc_bn,update_ema_fc=self.batchnorm(h_fc,beta_fc,convolutional=False)
+            self.update_emas.append(update_ema_fc)
+            self.fc_bn_relu=tf.nn.relu(fc_bn,name='relu')
+            fc_bn_drop=tf.nn.dropout(self.fc_bn_relu,self.keep_prob)
+        with tf.variable_scope('out_layer'):
+            W_out=self.weight_variable([self.fc_hidden_size,self.n_class],name='Weight_out')
+            tf.summary.histogram('Weight_out',W_out)
+            b_out=self.bias_variable([self.n_class],name='bias_out')
+            tf.summary.histogram('bias_out',b_out)
+            self._y_pred=tf.nn.xw_plus_b(fc_bn_drop,W_out,b_out,name='y_pred')
+        with tf.name_scope('loss'):
+            self._loss=tf.reduce_mean(
+                    tf.nn.sigmoid_cross_entropy_with_logits(logits=self._y_pred,labels=self._y_inputs))
+            tf.summary.scalar('loss',self._loss)
+        self.saver=tf.train.Saver(max_to_keep=2)
+    @property
+    def tst(self):
+        return self._tst
+    
+    @property
+    def keep_pro(self):
+        return self._keep_prob
+    
+    @property
+    def batch_size(self):
+        return self._batch_size
+    
+    @property
+    def global_step(self):
+        return self._global_step
+    
+    @property
+    def X1_inputs(self):
+        return self._X1_inputs
+    
+    @propery
+    def X2_inputs(self):
+        return self._X2_inputs
+    
+    @property
+    def y_inputs(self):
+        return self._y_inputs
+    
+    @property
+    def y_pred(self):
+        return self._y_pred
+    
+    @propery
+    def loss(self):
+        return self._loss
+    
+    
     def weight_variable(self,shape,name):    
         initial=tf.truncated_normal(shape,stddev=0.1)
         return tf.Variable(initial,name=name)
@@ -83,18 +141,14 @@ class TextCNN():
         bnepsilon=1e-5
         if convolutional:
             #以某一维度计算均值或方差
-            mean,variance=tf.nn.moments(Ylogits,[0,1,2])
+            mean,variance=tf.nn.moments(Ylogits,[0,1,2])#只剩下卷积核那个维度的维数了
         else:
             mean,variance=tf.nn.moments(Ylogits,[0])
         update_moving_averages=exp_moving_avg.apply([mean,variance])
         tf.cond(self.tst,lambda: exp_moving_avg.average(mean), lambda: mean)
         tf.cond(self.tst,lambda: exp_moving_avg.average(variance), lambda: variance)
-        Ybn=tf.nn.batch_normalization(Ylogits,m,v,None,bnepsilon)
+        Ybn=tf.nn.batch_normalization(Ylogits,m,v,offset,None,bnepsilon)
         return Ybn,update_moving_averages
-        
-
-
-        
         
         
     def cnn_inference(self,X_inputs,n_step):
@@ -108,13 +162,24 @@ class TextCNN():
         inputs=tf.expand_dims(input,-1)#batch_size, n_step,embedding_size,1
         pooled_outputs=list()
         for i,filter_size in enumerate(self.filter_sizes):
-            filter_shape=[filter_size,self.embedding_size,1,self.n_filter]
-            W_filter=self.weight_variable(shape=filter_shape,name='W_filter')
-            beta=self.bias_variable(shape=[self.n_filter],name='beta_filter')
-            tf.summary.histogram('beta',beta)
-            conv=tf.nn.conv2d(inputs,W_filter,strides=[1,1,1,1],padding='VALID',name='conv')+beta
-            #激活层前面加BN
-            conv_bn,update_emas=self.batchnorm(conv,beta,convolutional=True)
+            with tf.variable_scope('conv-maxpool-%s'%filter_size):  
+                filter_shape=[filter_size,self.embedding_size,1,self.n_filter]
+                W_filter=self.weight_variable(shape=filter_shape,name='W_filter')
+                beta=self.bias_variable(shape=[self.n_filter],name='beta_filter')
+                tf.summary.histogram('beta',beta)
+                conv=tf.nn.conv2d(inputs,W_filter,strides=[1,1,1,1],padding='VALID',name='conv')
+                #激活层前面加BN,注意
+                conv_bn,update_emas=self.batchnorm(conv,beta,convolutional=True)
+                #激活层
+                h=tf.nn.relu(conv_bn,name="relu")
+                #池化层
+                pooled=tf.nn.max_pool(h,ksize=[1,n_step-filter_size+1,1,1],strides=[1,1,1,1],padding='VALID',name='pool')
+                pooled_outputs.append(pooled)#shape of pooled [1,1,1,X]
+                self.update_emas.append(update_emas)
+        h_pool=tf.concate(pooled_outputs,3)
+        h_pool_flat=tf.reshape(h_pool,[-1,self.n_filter_total])
+        return h_pool_flat#batch_size（一批有几条数据）*n_filter_total(一条数据可以获得的最大池化值)
+        
         
         
         
