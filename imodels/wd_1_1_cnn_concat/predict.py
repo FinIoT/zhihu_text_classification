c5a4b81cf2592141eef0ba23be40a589edf69e6a
FinIoT <liukesheng227@aliyun.com>
2018-07-30 09:44:07 -0500

    Add files via upload

--- imodels/wd_1_1_cnn_concat/predict.py
+++ imodels/wd_1_1_cnn_concat/predict.py
@@ -7,12 +7,42 @@ Created on Wed Jul 25 20:58:33 2018
 import tensorflow as tf
 import numpy as np
 import os
-
+import time
 import network
+import tqdm
 
 settings=network.Settings()
+title_len=settings.title_len
 ckpt_path=settings.ckpt_path
+
 embedding_path = '../../data/word_embedding.npy'
+data_valid_path = '../../data/wd-data/data_valid/'
+
+va_batches=os.listdir(data_valid_path)
+n_va_batches=len(va_batches)
+
+def get_batch(batch_id):
+    new_batch=np.load(data_valid_path+str(batch_id)+'.npz')
+    X_batch=new_batch['X']
+    y_batch=new_batch['y']
+    X1_batch=X_batch[:,:title_len]
+    X2_batch=X_batch[:,title_len:]
+    return [X1_batch,X2_batch,y_batch]
+
+def local_predict(sess, model):
+    #test on the valid data
+    time0=time.time()
+    predict_label_list=list()
+    marked_label_list=list()
+    predict_scores=list()
+    for i in tqdm(range(n_va_batches)):
+        [X1_batch, X2_batch, y_batch]=get_batch(i)
+        marked_label_list.extend(y_batch)
+        _batch_size=len(X1_batch)
+    
+    
+def predict(sess, model):
+    
 
 def main(_):
     #引入模型-先判断需要引入的路径存不存在
